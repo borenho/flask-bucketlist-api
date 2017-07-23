6b055d1dd5831f43b677294f3bf99f57a829764f
borenho <borecollins@gmail.com>
2017-07-23 20:25:16 +0300

    Create a new flask object using the config settings

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+"""
+This file creates a new Flask object, and returns it after it's loaded up with configuration
+settings using app.config and connected to the DB using db.init_app(app)
+"""
+from flask import Flask
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from config.config import app_config
+
+db = SQLAlchemy()
+
+def create_app(configuration):
+    app = Flask(__name__)
+    app.config.from_object(app_config[configuration])
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False    # This sets performance overhead, set to True for debugging
+
+    db.init_app(app)
+
+    return app

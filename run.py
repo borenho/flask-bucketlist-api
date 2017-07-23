cd381bd550abd4b3a24aeb2351fb1fc1939ea5c3
borenho <borecollins@gmail.com>
2017-07-23 20:30:34 +0300

    Create the app's entry point

---
+++ run.py
@@ -0,0 +1,13 @@
+"""This file define an entry point to start our app
+"""
+import os
+from app import create_app
+from flask_restful import Api
+
+configuration = os.getenv('APP_SETTINGS')    # configuration = 'development'
+app = create_app(configuration)
+
+
+if __name__ == '__main__':
+    app.run()
+ 
